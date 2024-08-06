2e28742d048637455175aca416b2df3c344cbc80
takuya-takebayashi <takegoog369@gmail.com>
2024-08-07 08:36:41 +0900

    Homeに表示するコンポーネントを作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import MonthlySummary from '../components/MonthlySummary'
+import Calendar from '../components/Calendar'
+import TransactionMenu from '../components/TransactionMenu'
+import TransactionForm from '../components/TransactionForm'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Box sx={{ display: "flex" }}>
+            {/* 左側のコンテンツ */}
+            <Box sx={{ flexGrow: 1 }}>
+                <MonthlySummary />
+                <Calendar />
+            </Box>
+            {/* 左側のコンテンツ */}
+            <Box>
+                <Box>
+                    <TransactionMenu />
+                    <TransactionForm />
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
