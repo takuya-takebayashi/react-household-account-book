bcf124005d1f331e8feef06d81dfb7cdc84a500b
takuya-takebayashi <takegoog369@gmail.com>
2024-08-06 23:06:44 +0900

    サイドバーのスタイルを変更

--- src/components/layout/common/SideBar.tsx
+++ src/components/layout/common/SideBar.tsx
@@ -1,16 +1,15 @@
 import { Box, Drawer } from '@mui/material';
 import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import Toolbar from '@mui/material/Toolbar';
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import EqualizerIcon from '@mui/icons-material/Equalizer';
+import { NavLink } from 'react-router-dom';
 
 interface SideBarProps {
     drawerWidth: number,
@@ -30,21 +29,39 @@ const SideBar = ({ drawerWidth, mobileOpen, handleDrawerToggle }: SideBarProps) 
         { text: "Report", path: "/report", icon: EqualizerIcon },
     ]
 
+    const baseLinkStyle: CSSProperties = {
+        textDecoration: "none",
+        color: "inherit",
+        display: "block"
+    }
+
+    const activeLinkStyle: CSSProperties = {
+        backgroundColor: "rgba(0,0,0,0.08)"
+    }
+
     const drawer = (
         <div>
             <Toolbar />
             <Divider />
             <List>
                 {menuItems.map((item, index) => (
-                    <ListItem key={index} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-                                <item.icon />
-                            </ListItemIcon>
-                            <ListItemText primary={item.text} />
-                        </ListItemButton>
-                    </ListItem>
+                    <NavLink key={item.text} to={item.path} style={({ isActive }) => {
+                        // console.log("選択されたメニューは", item.text, isActive);
+                        return {
+                            ...baseLinkStyle,
+                            ...(isActive ? activeLinkStyle : {})
+                        }
+                    }}>
+                        <ListItem key={index} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                                    <item.icon />
+                                </ListItemIcon>
+                                <ListItemText primary={item.text} />
+                            </ListItemButton>
+                        </ListItem>
+                    </NavLink>
                 ))}
             </List>
         </div>
